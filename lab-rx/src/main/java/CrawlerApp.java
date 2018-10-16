09fe960f124b45d6190fe5c0cb877f6467ff3a2b
Arcticae <tomekgsd@gmail.com>
2018-10-16 20:22:56 +0200

    Rxjava project skeleton

---
+++ lab-rx/src/main/java/CrawlerApp.java
@@ -0,0 +1,25 @@
+import io.reactivex.Observable;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class CrawlerApp  {
+
+    private static List<String> TOPICS = Arrays.asList("Agent Cooper", "Sherlock", "Poirot", "Miss Murple", "Detective Monk");
+
+
+    public static void main(String[] args) throws IOException {
+      //  PhotoCrawler photoCrawler = new PhotoCrawler();
+       // photoCrawler.resetLibrary();
+        //photoCrawler.downloadPhotoExamples();
+//        photoCrawler.downloadPhotosForQuery(TOPICS.get(0));
+//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+
+        Observable <Integer > stream = Observable.just(1,2,3,4,5);
+        stream.subscribe((Integer something ) -> System.out.println("i accept" + something));
+
+    }
+}
